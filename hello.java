1ece98943e2d4683152172e459fb35fc14203e20
Wayne Taranto <wtaranto@mitre.org>
2017-03-09 13:50:24 -0500

    Added 2 more output lines

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
    public static void main(String[] args) {
       // Prints "Hello Java" in the terminal window.
       System.out.println("Hello Modified Java");
+      System.out.println("Line 2");
+      System.out.println("Line 3");
    }
 }
