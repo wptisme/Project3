7f9d559f8ce58754b5c330c76cb99d9d8015a1ea
Wayne Taranto <wtaranto@mitre.org>
2017-03-09 13:48:31 -0500

    modified java output

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
    public static void main(String[] args) {
       // Prints "Hello Java" in the terminal window.
-      System.out.println("Hello Java");
+      System.out.println("Hello Modified Java");
    }
 }
